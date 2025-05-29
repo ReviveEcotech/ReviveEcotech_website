821ace338ca7e328ba767c63f3d5ad796f923349
kedhar123 <bkedhar@gitam.in>
2025-05-29 13:47:44 +0530

    Product Details Page

--- reco/src/app/layout.jsx
+++ reco/src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "../../components/footer";
+import Navbar from "../../components/navBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        {children}
+        <Navbar />
+        <main className="flex-1">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
