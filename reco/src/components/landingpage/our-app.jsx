1067d7acfb34b27b303df2eed6ed5a88fd096421
gaurav <gaurav1452001@gmail.com>
2025-05-31 22:02:04 +0530

    minor css

--- reco/src/components/landingpage/our-app.jsx
+++ reco/src/components/landingpage/our-app.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 const AppPreview = () => {
   return (
-    <div className='py-5 md:py-10'>
-      <div className="mt-2 text-black md:mt-12 bg-white rounded-3xl mx-4 lg:mx-20 py-5 md:py-7 px-3 md:px-10">
+    <div className='py-5 md:py-8'>
+      <div className="mt-2 text-black md:mt-12 bg-white rounded-3xl mx-4 lg:mx-20 py-5 md:py-7 px-3 md:px-24">
         <h2 className="text-3xl  md:text-[2.5rem] text-center font-bold mb-6 md:mb-2 relative">
           Download
           <span>
