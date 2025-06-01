93a0cbefd96efac13d7180907c2326db5dea714d
gaurav <gaurav1452001@gmail.com>
2025-06-01 16:30:38 +0530

    why choose revive fix

--- reco/src/components/landingpage/why-choose.jsx
+++ reco/src/components/landingpage/why-choose.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const WhyChooseUs = () => {
   return (
     <div className="my-24"> {/* Further reduced from my-16 to my-8 */}
-      <div className="fontcolorgradient text-center mb-28 md:-mb-20"> {/* Added mb-8 to reduce space */}
+      <div className="fontcolorgradient text-center mb-28"> {/* Added mb-8 to reduce space */}
         Why Choose Revives
       </div>
-      <div className='flex flex-col md:flex-row items-center justify-center gap-8 md:gap-10 lg:gap-20 xl:gap-28 px-10 md:px-8 lg:px-20 xl:px-36'>
-        <div className='relative mb-20 md:mb-0 flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-white bg-[#013C5A] rounded-4xl'> {/* Reduced pt-32 to pt-24 */}
+      <div className='flex flex-col md:flex-row flex-start justify-center gap-8 md:gap-10 lg:gap-20 xl:gap-28 px-10 md:px-8 lg:px-20 xl:px-36 '>
+        <div className='relative w-full self-start mb-20 md:mb-0 flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-white bg-[#013C5A] rounded-4xl'> {/* Reduced pt-32 to pt-24 */}
           {/* Rest of the code remains the same */}
           <div>
             <Image
@@ -24,7 +24,7 @@ const WhyChooseUs = () => {
           <div className='font-mono '>Our intuitive platform makes recycling accessible to everyone.</div>
         </div>
         {/* Similar changes for other boxes */}
-        <div className='relative mb-20 md:mb-0 md:mt-[10rem] flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-black bg-[#A7CB4F] rounded-4xl'> {/* Changed pt-40 to pt-32 */}
+        <div className='relative w-full self-start mb-20 md:mb-0 md:mt-[6rem] flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-black bg-[#A7CB4F] rounded-4xl'> {/* Changed pt-40 to pt-32 */}
           {/* Second box content */}
           <div>
             <Image
@@ -39,7 +39,7 @@ const WhyChooseUs = () => {
           <div className='font-bold text-2xl'>Rewards & Referrals</div>
           <div className='font-mono'>Earn points, discounts, and bonuses for recycling and referring friends.</div>
         </div>
-        <div className='relative mb-20 md:mb-0 md:mt-[25rem] flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-white bg-[#013C5A] rounded-4xl'> {/* Changed pt-40 to pt-32 */}
+        <div className='relative w-full self-start mb-20 md:mb-0 md:mt-[14rem] flex flex-col gap-6 pb-14 pt-32 px-2 lg:px-12 items-center justify-center text-center text-white bg-[#013C5A] rounded-4xl'> {/* Changed pt-40 to pt-32 */}
           {/* Third box content */}
           <div>
             <Image
